86e314248b69a6b5b6a6434b98245c7e8314ed82
mbaldwin21 <mbaldwin@students.mchenry.edu>
2025-09-04 11:07:22 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,17 @@
+# this is a single-line comment in python 
+# TODO message for others to know to do something later
+
+
+# Create a madlibs on the first verse of mary had a little lamb
+
+# Define variables for the madlibs
+
+tank_size = 20
+# Define the size
+distance = 800
+# Calculate when the tank will run out
+mpg  = distance / tank_size
+# Print the distance it can reach until empty
+print ("miles per gallon", mpg) 
+# Explain the calculation
+print ("In order to calculate how much mileage you'll get for 800 miles, you divide by the tank size of your vehicle")
